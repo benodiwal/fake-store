983fbd1bd95aca1a06f93f8db39eb456ac327c78
codygeek01 <s474996633@gmail.com>
2024-03-12 14:53:11 +0530

    Initialize the project

---
+++ src/main/java/com/benodiwal/fakestore/FakestoreApplication.java
@@ -0,0 +1,13 @@
+package com.benodiwal.fakestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benodiwal/fakestore/FakestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benodiwal.fakestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
